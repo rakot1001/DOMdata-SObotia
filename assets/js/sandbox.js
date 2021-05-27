5959d4592fe72ab357a3d008cd4a01f21835a5d6
Vlad <vladdp70@gmail.com>
2021-05-28 01:44:19 +0300

    Start

---
+++ assets/js/sandbox.js
@@ -0,0 +1,29 @@
+"use strict";
+function makeCounter() {
+  let count = 0; //замкнутая переменная инкапсулированая
+  return {
+    increment() {
+      return ++count;
+    },
+    decrement() {
+      return --count;
+    },
+  };
+}
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+/*
+Напишите функцию create Adder(n),
+которая вернет другую функцию.Возвращаемая функция должна складывать получаемый аргумент m
+с агргументом n возвращаемой функции.
+const adder(10)=20
+adder(50)=70
+adder(100)=170
+*/
+
+const createAdder = (n) => (m) => n +=m;;
+
+
+const adder = createAdder(100);
